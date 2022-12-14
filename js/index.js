21ab652a9c823a6e1463fa7e926b670d748ebb72
ParkSangEun2016212014 <71581398+ParkSangEun2016212014@users.noreply.github.com>
2022-12-14 18:24:30 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// dream2-ironman
+
+$(setTimeout(function () {
+
+    var userInput = prompt("꿈은 뭐였나요? \n장래희망 말고, 해보고 싶었던 걸로요.\n전 아이언맨을 만들고 싶었어요." + "")
+
+    var userInput = prompt("꿈 때문에 해본 일이 있으신가요?\n계획을 세워봤다던가요." + "")
+
+    alert("그 꿈을 이뤘길 바랄게요.")
+    location.href = "https://parksangeun2016212014.github.io/dream3";
+}, 1000));
